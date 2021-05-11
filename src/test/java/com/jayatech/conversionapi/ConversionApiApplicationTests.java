a4883aeb583b0bda7e3db88d553cf7c20f2db4f2
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2021-05-11 08:35:28 -0300

    first commit

---
+++ src/test/java/com/jayatech/conversionapi/ConversionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayatech.conversionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
